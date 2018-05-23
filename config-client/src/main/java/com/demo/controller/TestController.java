541e51226f53dd04c50e3a3432f2cc5f0ad14f1e
yaopchen <1768651511@qq.com>
2018-05-23 14:56:14 +0800

    test

--- config-client/src/main/java/com/demo/controller/TestController.java
+++ config-client/src/main/java/com/demo/controller/TestController.java
@@ -10,9 +10,15 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
 	@Value("${from}")
 	private String from;
+	@Value("${password}")
+	private String password;
 
 	@RequestMapping("/from")
 	public String from() {
 		return from;
+	}
+	@RequestMapping("/password")
+	public String password() {
+		return password;
 	}
 }
