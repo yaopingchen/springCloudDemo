6273049cabc6a07a2f1afff4a607e228ca1dcda2
yaopchen <1768651511@qq.com>
2018-05-29 13:06:49 +0800

    保护机制开关

--- out-service/src/main/java/com/demo/service/UserInfoService.java
+++ out-service/src/main/java/com/demo/service/UserInfoService.java
@@ -2,12 +2,7 @@ package com.demo.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.stream.annotation.EnableBinding;
-import org.springframework.context.annotation.Bean;
-import org.springframework.integration.annotation.InboundChannelAdapter;
-import org.springframework.integration.core.MessageSource;
 import org.springframework.integration.support.MessageBuilder;
-import org.springframework.messaging.Message;
-import org.springframework.messaging.support.GenericMessage;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
